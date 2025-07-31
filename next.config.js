787573b916405777b4c95bc5e30b49612016350f
Stoetbul94 <100117300+Stoetbul94@users.noreply.github.com>
2025-07-31 23:45:55 +0200

    fix: remove deprecated appDir config for Next.js 14+ compatibility

--- next.config.js
+++ next.config.js
@@ -7,18 +7,8 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  experimental: {
-    appDir: false,
-  },
+
 };
 
-// Temporarily disable Sentry for build testing
-const sentryWebpackPluginOptions = {
-  silent: true,
-  hideSourceMaps: true,
-  disableServerWebpackPlugin: true,
-  disableClientWebpackPlugin: true,
-};
-
-// Make sure adding Sentry options is the last code to run
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions); 
+// Sentry Webpack plugin temporarily disabled for local build troubleshooting
+module.exports = nextConfig; 
