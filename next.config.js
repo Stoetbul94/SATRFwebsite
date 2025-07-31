7a7ceb1c72e7bdfe29df61ae451e1b2b0f584537
Stoetbul94 <100117300+Stoetbul94@users.noreply.github.com>
2025-07-31 23:40:04 +0200

    chore: temporarily disable Sentry Webpack plugin for local build troubleshooting

--- next.config.js
+++ next.config.js
@@ -12,14 +12,12 @@ const nextConfig = {
   },
 };
 
+// Temporarily disable Sentry for build testing
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses source map uploading logs during build
+  silent: true,
+  hideSourceMaps: true,
+  disableServerWebpackPlugin: true,
+  disableClientWebpackPlugin: true,
 };
 
 // Make sure adding Sentry options is the last code to run
